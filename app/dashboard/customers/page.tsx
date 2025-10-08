87b8f23b8201216270932b0f9e03d77f3bde15c8
Valeriy Gurnak <vmgurnak@gmail.com>
2025-10-08 11:22:07 +0300

    created

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CustomersPage() {
+  return <div>Customers Page</div>;
+}
+
+export default CustomersPage;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function InvoicesPage() {
+  return <div>Invoices Page</div>;
+}
+
+export default InvoicesPage;
